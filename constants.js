4abfb395e21c7b7ca299a4f1301bdb8213e47ebc
Xiao Li <xiao.li@workstar.com.au>
2021-08-08 13:21:17 +1000

    move code to engine.js add interval

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const WORLD_WIDTH = 480;
+export const WORLD_HEIGHT = 240;
--- src/engine.js
+++ src/engine.js
@@ -1,8 +1,24 @@
+import { WORLD_WIDTH, WORLD_HEIGHT } from '../constants.js';
+
 // (world: boolean[][]) => boolean[][]
 export const next = (world) => {
   return world;
 };
 // (pattern: string) => boolean[][]
 export const parse = (pattern) => {
-  return [];
+  const world = [...Array(WORLD_HEIGHT)].map(a => Array(WORLD_WIDTH).fill(false));
+  const width = pattern[0].length;
+  const height = pattern.length;
+
+  const startX = Math.floor((WORLD_WIDTH - width) / 2);
+  const startY = Math.floor((WORLD_HEIGHT - height) / 2);
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (pattern[i][j] === 'O')
+        world[startY + i][startX + j] = true;
+    }
+  }
+
+  return world;
 };
